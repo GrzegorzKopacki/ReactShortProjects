ede3f24c6141a48638fc9f0cf65a94cc1fbb225d
Grzegorz <gkopacki92@gmail.com>
2024-04-28 17:39:10 +0200

    added handle for multiple selection in accordian component

--- ReactShortProjects/src/components/Accordian/Accordian.jsx
+++ ReactShortProjects/src/components/Accordian/Accordian.jsx
@@ -4,24 +4,52 @@ import data from "./data";
 
 export default function Accordian() {
 	const [selected, setSelected] = useState(null);
+	const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+	const [multiple, setMultiple] = useState([]);
 
 	function handleSingleSelection(getCurrentId) {
 		setSelected(getCurrentId === selected ? null : getCurrentId);
 	}
+	function handleMultiSelection(getCurrentId) {
+		let cpyMultiple = [...multiple];
+		const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+		if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+		else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+		setMultiple(cpyMultiple);
+	}
+
 	return (
 		<div className={styles.wrapper}>
+			<button
+				onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+				className={styles.MultiSelectionButton}
+			>
+				Enable Multi Selection
+			</button>
 			<div className={styles.accordian}>
 				{data && data.length > 0 ? (
 					data.map((dataItem) => (
 						<div key={dataItem.id} className={styles.item}>
 							<div
-								onClick={() => handleSingleSelection(dataItem.id)}
+								onClick={
+									enableMultiSelection
+										? () => handleMultiSelection(dataItem.id)
+										: () => handleSingleSelection(dataItem.id)
+								}
 								className={styles.title}
 							>
 								<h3>{dataItem.question}</h3>
 								<span>+</span>
 							</div>
-							{selected === dataItem.id ? <div className={styles.answer}>{dataItem.answer}</div> : null}
+							{enableMultiSelection
+								? multiple.indexOf(dataItem.id) !== -1 && (
+										<div className={styles.answer}>{dataItem.answer}</div>
+								  )
+								: selected === dataItem.id && (
+										<div className={styles.answer}>{dataItem.answer}</div>
+								  )}
 						</div>
 					))
 				) : (
