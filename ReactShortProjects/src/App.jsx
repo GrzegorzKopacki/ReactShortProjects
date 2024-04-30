ee140202808b9e67849f8a1de794d50228641b7f
Grzegorz <gkopacki92@gmail.com>
2024-04-30 11:45:43 +0200

    installed react icons

--- ReactShortProjects/src/App.jsx
+++ ReactShortProjects/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Accordian from "./components/Accordian/Accordian";
 import RandomColor from "./components/RandomColor/RandomColor";
+import StarRating from "./components/StarRating/StarRating";
 
 function App() {
 	return (
 		<>
 			{/* <Accordian /> */}
-			<RandomColor />
+			{/* <RandomColor /> */}
+			<StarRating/>
 		</>
 	);
 }
---
+++ ReactShortProjects/src/components/StarRating/StarRating.jsx
@@ -0,0 +1,6 @@
+import styles from "./StarRating.module.css"
+
+
+export default function StarRating() {
+    return <div></div>
+}
