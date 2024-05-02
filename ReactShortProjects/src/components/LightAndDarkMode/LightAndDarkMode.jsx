5e5fbb9e05ab106fcd596b803af51c182e8de3e9
Grzegorz <gkopacki92@gmail.com>
2024-05-03 00:18:57 +0200

    created handleToggleTheme function in lightandDarkmode component

--- ReactShortProjects/src/components/LightAndDarkMode/LightAndDarkMode.jsx
+++ ReactShortProjects/src/components/LightAndDarkMode/LightAndDarkMode.jsx
@@ -1,11 +1,17 @@
 import styles from "./LightAndDarkMode.module.css";
+import useLocalStorage from "./useLocalStorage";
 
 export default function LightAndDarkMode() {
+	const [theme, setTheme] = useLocalStorage("theme", "dark");
+
+	function handleToggleTheme() {
+		setTheme(theme === "light" ? "dark" : "light");
+	}
 	return (
 		<div className={styles.lightdarkmode}>
 			<div className={styles.container}>
 				<p>Hello World!</p>
-				<button>Change Theme</button>
+				<button onClick={() => handleToggleTheme}>Change Theme</button>
 			</div>
 		</div>
 	);
