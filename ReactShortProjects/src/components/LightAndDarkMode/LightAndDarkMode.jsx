d0f1ed2601626512344038b588df2042f4d13fb3
Grzegorz <gkopacki92@gmail.com>
2024-05-03 00:14:59 +0200

    created useLocalStorage customHook

--- ReactShortProjects/src/components/LightAndDarkMode/LightAndDarkMode.jsx
+++ ReactShortProjects/src/components/LightAndDarkMode/LightAndDarkMode.jsx
@@ -1,5 +1,12 @@
 import styles from "./LightAndDarkMode.module.css";
 
 export default function LightAndDarkMode() {
-	return <div></div>;
+	return (
+		<div className={styles.lightdarkmode}>
+			<div className={styles.container}>
+				<p>Hello World!</p>
+				<button>Change Theme</button>
+			</div>
+		</div>
+	);
 }
