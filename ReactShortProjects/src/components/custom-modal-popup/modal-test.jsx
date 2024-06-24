a5db2f593ef41a6ba25b1c4b5d33d87b2f029b43
Grzegorz <gkopacki92@gmail.com>
2024-06-24 21:20:28 +0200

    added onClose function in modaltest component

--- ReactShortProjects/src/components/custom-modal-popup/modal-test.jsx
+++ ReactShortProjects/src/components/custom-modal-popup/modal-test.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Modal from "./modal";
-import "./modal.css"
+import "./modal.css";
 
 export default function ModalTest() {
 	const [showModalPopup, setShowModalPopup] = useState(false);
@@ -8,10 +8,16 @@ export default function ModalTest() {
 	function handleToggleModalPopup() {
 		setShowModalPopup(!showModalPopup);
 	}
+
+	function onClose() {
+		setShowModalPopup(false);
+	}
 	return (
 		<div>
 			<button onClick={handleToggleModalPopup}>Open Modal Popup</button>
-			{showModalPopup && <Modal body={<div>Customized body</div>} />}
+			{showModalPopup && (
+				<Modal onClose={onClose} body={<div>Customized body</div>} />
+			)}
 		</div>
 	);
 }
