c252b083554d3c12800e45e66bef72eb087b90fa
Grzegorz <gkopacki92@gmail.com>
2024-06-24 21:15:23 +0200

    added props for modal component

--- ReactShortProjects/src/components/custom-modal-popup/modal.jsx
+++ ReactShortProjects/src/components/custom-modal-popup/modal.jsx
@@ -1,10 +1,21 @@
-export default function Modal({ id }) {
+export default function Modal({ id, header, body, footer }) {
 	return (
 		<div id={id || "Modal"} className="modal">
 			<div className="content">
-				<div className="header"></div>
-				<div className="body"></div>
-				<div className="footer"></div>
+				<div className="header">
+					<span className="close-modal-icon">&times;</span>
+					<h2>{header ? header : "Header"}</h2>
+				</div>
+				<div className="body">
+					{body ? (
+						body
+					) : (
+						<div>
+							<p>This is our Modal Body</p>
+						</div>
+					)}
+				</div>
+				<div className="footer">{footer ? footer : <h2>Footer</h2>}</div>
 			</div>
 		</div>
 	);
