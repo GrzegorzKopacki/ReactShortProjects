220b295a866393fcfae2e5b944fd096cba65dd6e
Grzegorz <gkopacki92@gmail.com>
2024-07-02 06:00:51 +0200

    created new component searchbarwith autocomplete

---
+++ ReactShortProjects/src/components/SearchBarWithApi/index.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+export default function SearchAutocomplete() {
+	const [loading, setLoading] = useState(false);
+	const [users, setUsers] = useState([]);
+	const [error, setError] = useState(null);
+
+	async function fetchListOfUsers() {
+		try {
+			const response = await fetch("https://dummyjson.com/users")
+            const data = await response.json()
+				
+		} catch (error) {
+			console.log(error);
+			setError(error);
+		}
+	}
+
+	useEffect(() => {
+		fetchListOfUsers();
+	}, []);
+	return (
+		<div className="search-autocomplete-container">
+			<input
+				type="text"
+				name="search-user"
+				placeholder="Search Users here..."
+			/>
+		</div>
+	);
+}
