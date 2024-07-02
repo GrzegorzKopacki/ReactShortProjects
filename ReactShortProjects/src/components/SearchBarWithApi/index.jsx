f348aba65fca3bf8f428725c91865c851a6a89eb
Grzegorz <gkopacki92@gmail.com>
2024-07-02 06:08:29 +0200

    created new function handleChange in seearchautocomplete component

--- ReactShortProjects/src/components/SearchBarWithApi/index.jsx
+++ ReactShortProjects/src/components/SearchBarWithApi/index.jsx
@@ -4,13 +4,29 @@ export default function SearchAutocomplete() {
 	const [loading, setLoading] = useState(false);
 	const [users, setUsers] = useState([]);
 	const [error, setError] = useState(null);
+	const [searchParam, setSearchParam] = useState("");
+	const [showDropdown, setShowDropdown] = useState(false);
+	const [filteredUsers, setFilteredUsers] = useState([]);
+
+	function handleChange(event) {
+		const query = event.target.value.toLowerCase();
+		setSearchParam(query);
+
+		if (query > 1) {
+		}
+	}
 
 	async function fetchListOfUsers() {
 		try {
-			const response = await fetch("https://dummyjson.com/users")
-            const data = await response.json()
-				
+			const response = await fetch("https://dummyjson.com/users");
+			const data = await response.json();
+			if (data && data.users && data.users.length) {
+				setUsers(data.users.map((userItem) => userItem.firstName));
+				setLoading(false);
+				setError(null);
+			}
 		} catch (error) {
+			setLoading(false);
 			console.log(error);
 			setError(error);
 		}
@@ -22,9 +38,10 @@ export default function SearchAutocomplete() {
 	return (
 		<div className="search-autocomplete-container">
 			<input
-				type="text"
 				name="search-user"
 				placeholder="Search Users here..."
+				value={searchParam}
+				onChange={handleChange}
 			/>
 		</div>
 	);
