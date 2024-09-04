767991cad68dabc42f08ddc0fd5538009b7fbbe6
Grzegorz <gkopacki92@gmail.com>
2024-09-04 13:15:39 +0200

    created new folder and files for feature-flag project

--- ReactShortProjects/src/App.jsx
+++ ReactShortProjects/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 			{/* <ModalTest/> */}
 			{/* <GithubProfileFinder /> */}
 			{/* <SearchAutocomplete/> */}
-			<TicTacToe/>
+			{/* <TicTacToe/> */}
 			
 		</div>
 	);
---
+++ ReactShortProjects/src/components/feature-flag/data.js
@@ -0,0 +1,16 @@
+const dummyApiResponse = {
+	showLightAndDarkMode: false,
+	showTicTacTieBoard: true,
+	showRandomColorGenerator: true,
+	showAccordian: false,
+	showTreeView: true,
+};
+
+
+function FeatureFlagsDataServiceCall() {
+    return new Promise((resolve, reject) => {
+        if(dummyApiResponse) setTimeout(resolve(dummyApiResponse), 500);
+        else reject('Some error occured! Please try again')
+    })
+
+}
